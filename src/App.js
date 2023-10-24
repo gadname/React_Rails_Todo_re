82d3656c66226bf6deee15e27561909f9abe1370
gadname <gakugaku1114@icloud.com>
2023-10-24 23:08:01 +0900

    イベントハンドラ登録

--- src/App.js
+++ src/App.js
@@ -5,6 +5,34 @@ import { Center, Box, CheckboxGroup, Text } from "@chakra-ui/react";
 
 
 const App = () => {
+  const initialTasks = [
+    {
+      name: "買い物",
+      isDone: true,
+    },
+    {
+      name: "ランニング",
+      isDone: false,
+
+    },
+    {
+      name: "プログラミングの勉強",
+      isDone: false,
+    },
+  ];
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(initialTasks);
+  }, []);
+
+  const toggleIsDone = (index) => {
+    const tasksCopy = [...tasks];
+    const isDone = tasksCopy[index].isDone;
+    tasksCopy[index].isDone = !isDone;
+    setTasks(tasksCopy)
+  }
+
   return (
     <Box mt="64px">
       <Center>
