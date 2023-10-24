5674f94d21b03c22c4e0cea0e4305ae645b492cd
gadname <gakugaku1114@icloud.com>
2023-10-24 22:38:20 +0900

    AppコンポーネントのUI構築

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import Task from './component/Task';
+import { Center, Box, CheckboxGroup, Text } from "@chakra-ui/react";
+
 
 
 const App = () => {
   return (
-    <>
-    <h1>タスク一覧</h1>
+    <Box mt="64px">
+      <Center>
+        <Box>
+        <Box mb="24px">
+          <Text fontSize="24px" fontWeight="bold">
+    タスク一覧
+    </Text>
+    </Box>
+          <CheckboxGroup>
     <Task name="買い物" /> 
     <Task name="ランニング" />
     <Task name="プログラミングの基礎" />
-    </>
-    
+    </CheckboxGroup>
+        </Box>
+      </Center>
+    </Box>
   );
 }
 
